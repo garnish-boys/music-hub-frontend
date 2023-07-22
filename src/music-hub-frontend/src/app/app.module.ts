19d390a54e3087f626a3dd398ed955076f2375a5
ajharf <ajharf@gmail.com>
2023-07-22 01:08:42 -0500

    Created navbar using angular material including icon, My Projects tab, and 'Hello Andrew' tab with profile dropdown options

--- src/music-hub-frontend/src/app/app.module.ts
+++ src/music-hub-frontend/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+//Below are our angular material modules. We should organize these imports
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
