757fbc1d6457ea0d32c9d730f855a5e058426ce7
ajharf <ajharf@gmail.com>
2023-07-31 16:43:03 -0500

    Setup registration ui, need to add more fields and wire to auth service

--- src/music-hub-frontend/src/app/app.routes.ts
+++ src/music-hub-frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 export const routes: Routes = [
     { title: 'Home', path: 'home', component: HomeComponent },
     { title: 'About', path: 'about', component: AboutComponent },
+    { title: 'Registration', path: 'registration', component: RegistrationComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/music-hub-frontend/src/app/registration/registration.component.ts
+++ src/music-hub-frontend/src/app/registration/registration.component.ts
@@ -1,13 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, Validators, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { ContentWrapperComponent } from '../content-wrapper/content-wrapper.component';
+
+
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ContentWrapperComponent, MatCardModule, MatIconModule, MatInputModule, MatToolbarModule, ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit{
+  registerForm: FormGroup;
+  fieldRequired: string = "This field is required";
+  // private auth = inject(AuthService);
 
+  constructor() { }
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm(){
+    let emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    this.registerForm = new FormGroup(
+      {
+        'username': new FormControl(null,[Validators.required]),
+        'email': new FormControl(null,[Validators.required, Validators.pattern(emailregex)]),
+        'password': new FormControl(null, [Validators.required, this.checkPassword]),
+      });
+  }
+
+  emailErrors() {
+    return this.registerForm.get('email')?.hasError('required') ? 'This field is required' : this.registerForm.get('email')?.hasError('pattern') ? 'Not a valid emailaddress' :'';
+  }
+
+  checkPassword(control: any) {
+    let enteredPassword = control.value;
+    let passwordCheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})/;
+    return (!passwordCheck.test(enteredPassword) && enteredPassword) ? { 'requirements': true } : null;
+  }
+
+  getErrorPassword() {
+    return this.registerForm.get('password')?.hasError('required') ? 'This field is required (The password must be at least six characters, one uppercase letter and one number)' : this.registerForm.get('password')?.hasError('requirements') ? 'Password needs to be at least six characters, one uppercase letter and one number' : '';
+  }
+
+  checkValidation(input: string){
+    const validation = this.registerForm.get(input)?.invalid && (this.registerForm.get(input)?.dirty || this.registerForm.get(input)?.touched);
+    return validation;
+  }
+
+  // onSubmit(formData: FormGroup, formDirective: FormGroupDirective): void {   
+  //   const email = formData.value.email;
+  //   const password = formData.value.password;
+  //   const username = formData.value.username;
+  //   this.auth.registerUser(email, password, username);
+  //   formDirective.resetForm();
+  //   this.registerForm.reset();
+  // }
 }
