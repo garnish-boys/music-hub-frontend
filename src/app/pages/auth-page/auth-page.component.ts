0691d43a09b6c08eb8ea3246d58113503f5c4c09
Tyler Simmons <24759871+tyler-simmons@users.noreply.github.com>
2024-09-21 00:46:08 -0400

    login register ui

--- src/app/pages/auth-page/auth-page.component.ts
+++ src/app/pages/auth-page/auth-page.component.ts
@@ -5,11 +5,14 @@ import { RouterLink } from '@angular/router';
 import { HeaderOnlyLayoutComponent } from '@app/shared/layout/header-only-layout/header-only-layout.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-auth-page',
   standalone: true,
-  imports: [HeaderOnlyLayoutComponent, MatButtonModule, RouterLink, MatDividerModule, MatCardModule, MatTabsModule],
+  imports: [HeaderOnlyLayoutComponent, MatButtonModule, RouterLink, MatDividerModule, MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './auth-page.component.html',
   styleUrl: './auth-page.component.scss'
 })
